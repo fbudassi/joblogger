eca121ff5dd56463655c863c57ec5553b58d86a4
Federico Budassi <federico.budassi@rotterdam-cs.com>
2016-02-22 01:01:36 -0300

    Changed database table schema.

--- src/test/resources/DatabaseHandler.sql
+++ src/test/resources/DatabaseHandler.sql
@@ -1,10 +1,8 @@
 CREATE TABLE joblogger (
   logTime timestamp NOT NULL,
-  level integer NOT NULL,
-  logger varchar(64) NOT NULL,
+  level varchar(32) NOT NULL,
+  logger varchar(255) NOT NULL,
   message varchar(255) NOT NULL,
   sequence integer NOT NULL,
-  sourceClass varchar(64) NOT NULL,
-  sourceMethod varchar(32) NOT NULL,
   threadID integer NOT NULL,
   stackTrace varchar(8192));
